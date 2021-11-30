c395f9ae76c594c63d34e7b553d83d62ea7191ca
claudiu <k1mucl00@students.oamk.fi>
2021-11-30 16:22:23 +0200

    I've updated the app with a database and a user profile with functions that calculate how many calories do you need and what is your level of body based on BMI.

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -3,32 +3,75 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText name,age,height,weight;
-    Button next;
+public class MainActivity extends AppCompatActivity
+{
+    Button start;
+    UserDatabase db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        next = (Button) findViewById(R.id.nextbtn);
-        next.setOnClickListener(new View.OnClickListener() {
+        start =(Button) findViewById(R.id.btnstart);
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openQuestionnaire2();
+            public void onClick(View v)
+            {
+                openStart();
+                finish();
             }
         });
+
+
     }
-    public void openQuestionnaire2()
+    public void openStart()
     {
-        Intent intent = new Intent(this, Questionnaire2.class );
+        Intent intent = new Intent(this, StartActivity.class);
         startActivity(intent);
     }
+
+     public void onStart()
+    {
+        super.onStart();
+        db = new UserDatabase(this);
+        Cursor res = db.getdata();
+
+        StringBuffer buffer5 = new StringBuffer();
+        StringBuffer buffer6 = new StringBuffer();
+        StringBuffer buffer7 = new StringBuffer();
+
+        while (res.moveToNext())
+        {
+            buffer5.append(res.getString(5));
+            buffer6.append(res.getString(6));
+            buffer7.append(res.getString(7));
+        }
+
+        String goal= buffer5.toString();
+        String body_type = buffer6.toString();
+        String activity = buffer7.toString();
+
+        Boolean fan1 = goal.equals("null");
+        Boolean fan2 = body_type.equals("null");
+        Boolean fan3 = activity.equals("null");
+
+        if( res.getCount() != 0 && fan1 == false && fan2 == false && fan3 == false )
+        {
+            Intent i = new Intent(MainActivity.this,ProfileMainPage.class);
+            startActivity(i);
+            finish();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/fitnessapp/Questionnaire2.java
+++ app/src/main/java/com/example/fitnessapp/Questionnaire2.java
@@ -3,14 +3,20 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Questionnaire2 extends AppCompatActivity {
 
     Button back,next;
+    RadioGroup rg;
+    RadioButton rb;
+    UserDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +25,56 @@ public class Questionnaire2 extends AppCompatActivity {
 
         back = (Button) findViewById(R.id.backbtn);
         next = (Button) findViewById(R.id.nextbtn);
+        rg = (RadioGroup)findViewById(R.id.radiogroup);
+        db = new UserDatabase(this);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openMainActivity();
-            }
-        });
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openQuestionnaire3();
+            public void onClick(View v)
+            {
+                openStart();
             }
         });
 
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if(rg.getCheckedRadioButtonId()== -1 )
+                {
+                    Toast.makeText(Questionnaire2.this, "Please Select Your Goal!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Cursor res = db.getdata();
+                    StringBuffer buffer = new StringBuffer();
+                    while (res.moveToNext())
+                    {
+                        buffer.append(res.getString(0));
+                    }
+                    String name = buffer.toString();
+
+                    int id = rg.getCheckedRadioButtonId();
+                    rb = (RadioButton)findViewById(id);
+                    String goal = String.valueOf(rb.getText());
+
+                    db.addGoal(name,goal);
+
+                    openQuestionnaire3();
+                    finish();
+                }
+            }
+        });
     }
-    public void openMainActivity()
+    public void openStart()
     {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, StartActivity.class);
         startActivity(intent);
     }
     public void openQuestionnaire3()
     {
-        Intent intent = new Intent(this, Questionnaire3.class );
+        Intent intent = new Intent(this, Questionnaire3.class);
         startActivity(intent);
     }
+
 }
